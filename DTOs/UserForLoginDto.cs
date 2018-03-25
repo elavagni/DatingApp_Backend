33d0b64bc73857c9da6a3ba058d0d77e8a360288
eric <elavagnim@gmail.com>
2018-03-25 13:58:43 -0500

    Added validation code

--- DTOs/UserForLoginDto.cs
+++ DTOs/UserForLoginDto.cs
@@ -4,8 +4,12 @@ namespace DatingApp.API.DTOs
 {
     public class UserForLoginDto
     {       
+         [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a username between 4 and 8 characters")]        
         public string UserName {set;get;}
       
+       [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]        
         public string Password {set;get;}
     }
 }
